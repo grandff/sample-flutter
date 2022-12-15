a57591a23facc8bab1b669726ace7e1868153dc9
grandff <dsaqaz421@naver.com>
2022-12-15 22:50:15 +0900

    Feat : 3.0 Header

--- README.md
+++ README.md
@@ -6,6 +6,9 @@ flutter create XXX
 ## 필수 익스텐션
 Dart, Flutter 설치
 
+## To-Do list
+[ ] Dribble 보고 똑같이 .. 만들어보기
+
 ## 프로젝트 명령어 참조 
 (명명 규칙)[https://dart.dev/tools/pub/pubspec#name] </br>
 
@@ -21,3 +24,9 @@ Dart, Flutter 설치
 - 앱의 테마를 정해야함(MaterialApp, CupertinoApp)
 - 각각 안드로이드, iOS테마인데 구글에서 플러터를 만들다보니 MateriApp이 더 깔끔함
 - 모든 화면은 Scaffold 안에 들어가야함
+
+### 3.0 Header
+- 여러 위젯들을 써볼거임
+- 디자인을 직접 구현 (http://dribbble.com/shots/19858341-Finnancial-Mobile-IOS-App)
+- appBar 삭제하기
+  - appBar의 속성을 보면 ?가 붙어있음.. 필수가 아니란 뜻
--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,42 @@ class App extends StatelessWidget{  // core 위젯 중 하나를 상속받음
     // 메인 테마 설정
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title : Text('Hello Flutter')
-        ),
-        body: Text('Hello World!'),
+        backgroundColor: Color(0xFF181818),   // 커스텀배경색지정. 0xFF 로 시작
+        body: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 40
+          ),  
+          child: Column(
+            children: [
+              SizedBox( // 말그대로 사이즈가 있는 박스.. div같은
+                height: 80,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end, // 해당 ROW의 요소들을 맨끝으로 정렬
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end, // 텍스트의 끝으로 정렬. 이 옵션을 안주면 두번쨰의 Welcome back 정렬이 이상해짐
+                    children: [
+                    Text('Hey, Selena',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 28,
+                        fontWeight: FontWeight.w800
+                      ),
+                    ),
+                    Text('Welcome back',
+                      style: TextStyle(
+                        color:Colors.white.withOpacity(0.8),
+                        fontSize: 18
+                      ),
+                    ),
+                  ],)
+                ],
+              )
+            ],
+          ),
+        )
+        
       ),
     );
   }
