97d75db18d614ef540e541deabf7a99937bc9577
grandff <dsaqaz421@naver.com>
2022-12-28 13:02:06 +0900

    Feat : practice 2 design

--- practice_flutter2/lib/screens/home_screen.dart
+++ practice_flutter2/lib/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
 
@@ -9,40 +11,81 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  static const oneMinutes = 60;
+  int totalSeconds = oneMinutes;
+  int totalPomodoros = 0;
+  double totalProgress = 0.0;
+  bool isRunning = false;
+  late Timer timer;
+
+  // 타이머 포맷
+  String format(int seconds) {
+    var duration = Duration(seconds: seconds);
+    return duration.toString().split(".").first.substring(2);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          '시계',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
       backgroundColor: Theme.of(context).backgroundColor,
       body: Column(
         children: [
           Flexible(
-            flex: 3,
+            flex: 2,
             fit: FlexFit.tight,
             child: Container(
               // 이거 위젯으로 빼기
+              alignment: Alignment.bottomCenter,
               child: CircularPercentIndicator(
                 animation: true,
                 radius: 140.0, // 이게 크기임..
                 lineWidth: 15,
-                percent: 0.5,
-                center: const Text('20:21'),
+                percent: totalProgress,
+                center: Text(format(totalSeconds),
+                    style: const TextStyle(
+                      fontSize: 60,
+                    )),
                 backgroundColor: Colors.grey,
                 progressColor: Colors.orange,
               ),
             ),
           ),
           Flexible(
-            flex: 2,
+            flex: 1,
             fit: FlexFit.tight,
             child: Container(
+              alignment: Alignment.center,
               child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  IconButton(
-                      onPressed: () {},
-                      icon: const Icon(Icons.play_circle_outline_outlined)),
-                  IconButton(
-                      onPressed: () {},
-                      icon: const Icon(Icons.stop_circle_outlined)),
+                  Flexible(
+                    flex: 1,
+                    fit: FlexFit.tight,
+                    child: IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.play_circle_outline_outlined,
+                          size: 80,
+                        )),
+                  ),
+                  Flexible(
+                    flex: 1,
+                    fit: FlexFit.tight,
+                    child: IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.stop_circle_outlined,
+                          size: 80,
+                        )),
+                  ),
                 ],
               ),
             ),
@@ -52,14 +95,36 @@ class _HomeScreenState extends State<HomeScreen> {
             fit: FlexFit.tight,
             child: Container(
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   // 아래 버튼들 위젯으로 빼기
                   Container(
-                    child: const Text("Add One Minute"),
+                    width: 180,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        width: 2,
+                      ),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    padding: const EdgeInsets.all(10),
+                    child: const Text("1분 추가"),
                   ),
                   Container(
-                    child: const Text("History"),
+                    width: 180,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        width: 2,
+                      ),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    padding: const EdgeInsets.all(10),
+                    child: const Text("기록하기"),
                   ),
+                  Container(
+                    child: Column(
+                      children: const [Text("전체 횟수"), Text("0")],
+                    ),
+                  )
                 ],
               ),
             ),
