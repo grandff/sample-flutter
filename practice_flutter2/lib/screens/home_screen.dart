e2dcfaf4a9579f01269feed1b093b8401813dcf6
grandff <dsaqaz421@naver.com>
2022-12-28 22:45:48 +0900

    Feat : ontick practice

--- practice_flutter2/lib/screens/home_screen.dart
+++ practice_flutter2/lib/screens/home_screen.dart
@@ -15,23 +15,54 @@ class _HomeScreenState extends State<HomeScreen> {
   int totalSeconds = oneMinutes;
   int totalPomodoros = 0;
   double totalProgress = 0.0;
-  bool isRunning = false;
+  bool isRunning = false; // 현재 재생 중인지 일시정지상태인지
   late Timer timer;
+
+  // 타이머 시작
+  void onStart() {
+    timer = Timer.periodic(const Duration(seconds: 1), onTick);
+
+    // isRunning -> true 타이머 시작 알림 처리
+    setState(() {
+      isRunning = true;
+    });
+  }
+
+  // 타이머 처리
+  void onTick(Timer timer) {
+    setState(() {
+      totalSeconds = totalSeconds - 1;
+    });
+  }
+
+  // 타이머 초기화
+  void onStop() {}
+
+  // 타이머 일시정지
+  void onPause() {}
+
+  // 10초 추가
+  void addTenSeconds() {}
+
+  // 현재 시간 기록 후 다시 시작
+  void addHistory() {}
 
   // 타이머 포맷
   String format(int seconds) {
     var duration = Duration(seconds: seconds);
-    return duration.toString().split(".").first.substring(2);
+    return duration.toString();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          '시계',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
+        title: const Center(
+          child: Text(
+            '시계',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
           ),
         ),
       ),
@@ -40,7 +71,7 @@ class _HomeScreenState extends State<HomeScreen> {
         children: [
           Flexible(
             flex: 2,
-            fit: FlexFit.tight,
+            fit: FlexFit.loose,
             child: Container(
               // 이거 위젯으로 빼기
               alignment: Alignment.bottomCenter,
@@ -60,7 +91,7 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
           Flexible(
             flex: 1,
-            fit: FlexFit.tight,
+            fit: FlexFit.loose,
             child: Container(
               alignment: Alignment.center,
               child: Row(
@@ -70,9 +101,11 @@ class _HomeScreenState extends State<HomeScreen> {
                     flex: 1,
                     fit: FlexFit.tight,
                     child: IconButton(
-                        onPressed: () {},
-                        icon: const Icon(
-                          Icons.play_circle_outline_outlined,
+                        onPressed: !isRunning ? onStart : onPause,
+                        icon: Icon(
+                          !isRunning
+                              ? Icons.play_circle_outline_outlined
+                              : Icons.pause_circle_outline_outlined,
                           size: 80,
                         )),
                   ),
@@ -107,7 +140,8 @@ class _HomeScreenState extends State<HomeScreen> {
                       borderRadius: BorderRadius.circular(10),
                     ),
                     padding: const EdgeInsets.all(10),
-                    child: const Text("1분 추가"),
+                    alignment: Alignment.center,
+                    child: const Text("10초 추가"),
                   ),
                   Container(
                     width: 180,
@@ -118,11 +152,14 @@ class _HomeScreenState extends State<HomeScreen> {
                       borderRadius: BorderRadius.circular(10),
                     ),
                     padding: const EdgeInsets.all(10),
+                    alignment: Alignment.center,
                     child: const Text("기록하기"),
                   ),
-                  Container(
-                    child: Column(
-                      children: const [Text("전체 횟수"), Text("0")],
+                  Center(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: const [Text("전체 횟수 : "), Text("0")],
                     ),
                   )
                 ],
