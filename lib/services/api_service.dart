88e0542f2d09cab24cb3ab368a9d682b34455f0c
grandff <dsaqaz421@naver.com>
2023-01-04 12:43:33 +0900

    Feat: 6.12 ApiService

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:sample_flutter/models/webtoon_detail_model.dart';
+import 'package:sample_flutter/models/webtoon_episode_model.dart';
 import 'package:sample_flutter/models/webtoon_model.dart';
 
 class ApiService {
@@ -25,5 +27,35 @@ class ApiService {
     }
 
     throw Error(); // 정상이 아닌 경우 오류 처리
+  }
+
+  // 상세보기
+  static Future<WebtoonDetailModel> getToonsById(String id) async {
+    final url = Uri.parse("$baseUrl/$id");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      // json decode
+      final webtoon = jsonDecode(response.body);
+      return WebtoonDetailModel.fromJson(webtoon);
+    }
+
+    throw Error();
+  }
+
+  // 에피소드 불러오기
+  static Future<List<WebtoonEpisodeModel>> getLatestEpisodesById(
+      String id) async {
+    List<WebtoonEpisodeModel> episodesInstances = [];
+    final url = Uri.parse("$baseUrl/$id/episodes");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      // json decode
+      final episodes = jsonDecode(response.body);
+      for (var episode in episodes) {
+        episodesInstances.add(WebtoonEpisodeModel.fromJson(episode));
+      }
+      return episodesInstances;
+    }
+    throw Error();
   }
 }
